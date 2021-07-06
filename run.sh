3d4d486f58a99ae664b3cd9b4e0abbb1bc70f472
Eric Park <me@ericswpark.com>
2021-07-06 14:53:54 +0900

    run.sh: launch faster if venv is already initialized

--- run.sh
+++ run.sh
@@ -5,13 +5,14 @@ if [ ! -d "venv" ]
 then
     # Directory does not exist, initialize virtualenv
     virtualenv venv
+    . venv/bin/activate
+
+    # Install dependencies
+    pip3 install -r requirements.txt
+
+    python throttle-status.py
+else
+    # Directory exists, run immediately
+    . venv/bin/activate
+    python throttle-status.py
 fi
-
-# Activate virtualenv
-. venv/bin/activate
-
-# Check for any updates to requirements
-pip3 install -r requirements.txt
-
-# Start script
-python throttle-status.py
